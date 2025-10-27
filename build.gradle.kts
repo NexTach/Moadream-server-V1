e43ac227c98656de3a00f64a62d94217c00a4764
snowykte0426 <snowykte0426@naver.com>
2025-10-27 20:30:42 +0900

    add JWT configuration and deploy profile settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,11 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
     implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
 
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
     // Lombok
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
