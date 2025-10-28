75717680c0503346d53d1e6e06b674d05ef7980e
snowykte0426 <snowykte0426@naver.com>
2025-10-28 09:10:57 +0900

    add user role assignment in CustomUserDetailsService

--- src/main/java/com/nextech/moadream/server/v1/global/security/CustomUserDetailsService.java
+++ src/main/java/com/nextech/moadream/server/v1/global/security/CustomUserDetailsService.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 import java.util.Collections;
 
 @Service
@@ -24,7 +26,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         return org.springframework.security.core.userdetails.User.builder()
                 .username(user.getEmail())
                 .password(user.getPasswordHash())
-                .authorities(Collections.emptyList())
+                .authorities(Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")))
                 .build();
     }
 }
