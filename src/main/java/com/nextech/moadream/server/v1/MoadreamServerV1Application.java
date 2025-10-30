12426fd0f737b9f4216289a17b06773c2ce4907c
snowykte0426 <snowykte0426@naver.com>
2025-10-30 22:59:29 +0900

    reorder import statement for MegaMethod in MoadreamServerV1Application.java

--- src/main/java/com/nextech/moadream/server/v1/MoadreamServerV1Application.java
+++ src/main/java/com/nextech/moadream/server/v1/MoadreamServerV1Application.java
@@ -1,11 +1,12 @@
 package com.nextech.moadream.server.v1;
 
-import com.nextach.megamethod.MegaMethod;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.nextach.megamethod.MegaMethod;
 
 @SpringBootApplication
 @EnableJpaAuditing
