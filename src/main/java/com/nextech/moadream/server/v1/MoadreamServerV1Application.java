65b597249bda0f9dda4142b0670e60319995e63b
snowykte0426 <snowykte0426@naver.com>
2025-10-30 13:14:04 +0900

    enable JPA repositories for the application

--- src/main/java/com/nextech/moadream/server/v1/MoadreamServerV1Application.java
+++ src/main/java/com/nextech/moadream/server/v1/MoadreamServerV1Application.java
@@ -3,11 +3,13 @@ package com.nextech.moadream.server.v1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableJpaRepositories(basePackages = "com.nextech.moadream.server.v1.domain")
 public class MoadreamServerV1Application {
 
     public static void main(String[] args) {
