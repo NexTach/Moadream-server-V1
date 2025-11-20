acd6a9398cd49f015702534db717dd3167956d84
snowykte0426 <snowykte0426@naver.com>
2025-11-20 11:34:05 +0900

    enhance WebClientConfig with SSL context configuration and improved timeout settings

--- src/main/java/com/nextech/moadream/server/v1/global/config/WebClientConfig.java
+++ src/main/java/com/nextech/moadream/server/v1/global/config/WebClientConfig.java
@@ -3,12 +3,16 @@ package com.nextech.moadream.server.v1.global.config;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.SSLException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import io.netty.channel.ChannelOption;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import reactor.netty.http.client.HttpClient;
@@ -19,8 +23,16 @@ public class WebClientConfig {
     @Bean
     public WebClient webClient() {
         HttpClient httpClient = HttpClient.create().option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 30000)
-                .responseTimeout(Duration.ofSeconds(30))
-                .doOnConnected(conn -> conn.addHandlerLast(new ReadTimeoutHandler(30, TimeUnit.SECONDS))
+                .responseTimeout(Duration.ofSeconds(30)).secure(sslContextSpec -> {
+                    try {
+                        sslContextSpec
+                                .sslContext(SslContextBuilder.forClient()
+                                        .trustManager(InsecureTrustManagerFactory.INSTANCE).build())
+                                .handshakeTimeout(Duration.ofSeconds(30));
+                    } catch (SSLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).doOnConnected(conn -> conn.addHandlerLast(new ReadTimeoutHandler(30, TimeUnit.SECONDS))
                         .addHandlerLast(new WriteTimeoutHandler(30, TimeUnit.SECONDS)));
 
         return WebClient.builder().clientConnector(new ReactorClientHttpConnector(httpClient)).build();
